6d4a17cadf8e2763580648af79dcd03ed6c32e34
PulfordJ <jwpulf@essex.ac.uk>
2013-11-14 14:07:49 +0000

    Refined robot wrapper into two mouse and keyboard objects.

---
+++ server_work/src/MouseControl.java
@@ -0,0 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: john
+ * Date: 11/14/13
+ * Time: 1:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MouseControl {
+
+    public void moveMouse(String direction, int magnitude) {
+
+    }
+
+    public void leftMouseButtonToggle() {
+
+    }
+
+    public void leftMouseButtonHold() {
+
+    }
+
+    public void leftMouseButtonRelease() {
+
+    }
+
+
+}
