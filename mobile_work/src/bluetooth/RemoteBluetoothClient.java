e08bd3b5bf33a52ec77f434ceaacc170127e2649
vkujum <vkujum@essex.ac.uk>
2013-11-15 12:50:36 +0000

    Added RemoteBluetoothClient

---
+++ mobile_work/src/bluetooth/RemoteBluetoothClient.java
@@ -0,0 +1,39 @@
+public class RemoteBluetoothClient{
+	private	Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+	private	BluetoothAdapter deviceBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	public RemoteBluetoothClient(){			
+			if(hasBluetoothAdapter()){
+				if(!isBluetoothEnabled()){
+					turnBluetoothOn();	
+				}
+			}else{
+				//display message
+			}			
+			checkForPairedDevices();
+	}
+	private boolean hasBluetoothAdapter(){
+		if (deviceBluetoothAdapter == null) {
+			return false;
+		}
+		return true;
+	}
+		
+	private boolean isBluetoothEnabled(){	
+		return deviceBluetoothAdapter.isEnabled();
+	}
+	
+	private void turnBluetoothOn(){
+		if (!isBluetoothEnabled()) {
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+	}	
+	
+	private void checkForPairedDevices(){	
+		if (pairedDevices.size() > 0) {
+			for (BluetoothDevice device : pairedDevices) {
+				mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+			}
+		}	
+	}
+}
