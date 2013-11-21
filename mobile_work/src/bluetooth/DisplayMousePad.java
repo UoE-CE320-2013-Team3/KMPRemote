f024c81f9b322c063dcd09eaab80312d2a20c3ae
lgoodl <lgoodl@essex.ac.uk>
2013-11-21 15:32:09 +0000

    added methods in DisplayMousePad.java to test actions

--- mobile_work/src/bluetooth/DisplayMousePad.java
+++ mobile_work/src/bluetooth/DisplayMousePad.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class DisplayMousePad extends Activity {
 	RemoteBluetoothClient mclient;
 	//ConnectedThread t;
+	public static String cmd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mousepad_display);
-        mclient = new RemoteBluetoothClient();
-        mclient.connect();
+        //mclient = new RemoteBluetoothClient();
+        //mclient.mArrayAdapter = new ArrayAdapter<String>(this,R.layout.mousepad_display);
+        //ListView newDevicesListView = (ListView)()
+        //mclient.connect();
         
     }
 
@@ -28,49 +33,51 @@ public class DisplayMousePad extends Activity {
     //need to still implement these methods to send data to the bluetooth client class!
     public void moveUp(View v){
     	//call the bluetooth class and send MOVE_UP command
-    	String cmd = "MOUSE MOVE UP 20";
+    	cmd = "MOUSE MOVE UP 20";
     	byte[] buffer = cmd.getBytes();
-    	mclient.send(buffer);
-    	Toast.makeText(this,  "Message sent!", Toast.LENGTH_LONG).show();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
+    	
     }
     
     public void moveDown(View v){
-    	String cmd = "MOUSE MOVE UP 20";
+    	cmd = "MOUSE MOVE DOWN 20";
     	byte[] buffer = cmd.getBytes();
-    	mclient.send(buffer);
-    	Toast.makeText(this,  "Message sent!", Toast.LENGTH_LONG).show();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
+    	
     	
     }
 
 	public void moveLeft(View v){
 		String cmd = "MOUSE MOVE LEFT 20";
     	byte[] buffer = cmd.getBytes();
-    	mclient.send(buffer);
-    	Toast.makeText(this,  "Message sent!", Toast.LENGTH_LONG).show();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
 	
 	}
 
 	public void moveRight(View v){
 		String cmd = "MOUSE MOVE RIGHT 20";
     	byte[] buffer = cmd.getBytes();
-    	mclient.send(buffer);
-    	Toast.makeText(this,  "Message sent!", Toast.LENGTH_LONG).show();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
 	
 	}
 
 	public void leftClick(View v){
-		String cmd = "MOUSE MOVE UP 20";
+		String cmd = "MOUSE LEFT CLICK";
     	byte[] buffer = cmd.getBytes();
-    	mclient.send(buffer);
-    	Toast.makeText(this,  "Message sent!", Toast.LENGTH_LONG).show();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+ cmd, Toast.LENGTH_LONG).show();
 	
 	}
 
 	public void rightClick(View v){
-		String cmd = "MOUSE MOVE UP 20";
+		String cmd = "MOUSE RIGHT CLICK";
     	byte[] buffer = cmd.getBytes();
-    	mclient.send(buffer);
-    	Toast.makeText(this,  "Message sent!", Toast.LENGTH_LONG).show();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
 	
 	}
     
