4c7fcbf0cf0de3bd58d68f94188376549e110088
vkujum <vkujum@essex.ac.uk>
2013-11-21 14:35:02 +0000

    Fixed the connection between the list in the GUI and the array containing the bluetooth devices

--- mobile_work/src/bluetooth/RemoteBluetoothClient.java
+++ mobile_work/src/bluetooth/RemoteBluetoothClient.java
@@ -1,11 +1,11 @@
 package com.example.bluetoothconnection;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -13,30 +13,67 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.util.Log;
-import android.widget.Toast;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class RemoteBluetoothClient{
-	static UUID uuid = new UUID(0, 80087355);
+public class RemoteBluetoothClient extends Activity{
+	static UUID uuid = UUID.fromString("04c6093b-0000-1000-8000-00805f9b34fb");
 	
-	
+	//Variables for the adapter(phone's Bluetooth) and device(server Bluetooth).
 	private	BluetoothAdapter deviceBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-	private	Set<BluetoothDevice> pairedDevices = deviceBluetoothAdapter.getBondedDevices();
 	private BluetoothDevice btDevice;
-	//threads
-	private ConnectThread ct;
-	private ConnectedThread cnt;
+	private	Set<BluetoothDevice> pairedDevices = deviceBluetoothAdapter.getBondedDevices();	
+	public ArrayAdapter<String> mArrayAdapter;
+//	private BroadcastReceiver mReceiver;
 	
-	public RemoteBluetoothClient(){			
-			if(hasBluetoothAdapter()){
-				if(!isBluetoothEnabled()){
-					//warning mes
-					//Toast.makeText(this, "Turn Bluetooth on", Toast.LENGTH_SHORT).show();
-				}
-			}else{
+	//threads
+	private ConnectThread ct; //thread that creates the connection
+	private ConnectedThread cnt; //thread that manages the connection
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mArrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_main);
+		ListView newDevicesListView = (ListView)findViewById(R.id.listView1);
+		newDevicesListView.setAdapter(mArrayAdapter);
+		
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy		
+		
+		filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+		registerReceiver(mReceiver, filter);
+		
+		if(hasBluetoothAdapter()){
+			if(!isBluetoothEnabled()){
 				//display message
-			}			
+			}
+		}else{
+			//display message
+			getDevice();				
+		}	
+		newDevicesListView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(btDevice == null) {
+					//error
+				} else {
+					connect(btDevice);
+				}
+				
+			}
+			
+			
+		});
 	}
+	
+//	public RemoteBluetoothClient(){		
+//				
+//	}
+	
 	private boolean hasBluetoothAdapter(){
 		if (deviceBluetoothAdapter == null) {
 			return false;
@@ -46,49 +83,22 @@ public class RemoteBluetoothClient{
 		
 	public boolean isBluetoothEnabled(){	
 		return deviceBluetoothAdapter.isEnabled();
-	}
+	}	
 	
-	public void getDevice() {
-		boolean discovered = deviceBluetoothAdapter.startDiscovery();
-		
-		if(!discovered) {
-			while(!discovered){
-				Log.d("NULL", "Discovered is null: " +discovered);
-			}
-		}else{
-			Log.d("FOUND", "Discovered: " +discovered);
-			final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-		    public void onReceive(Context context, Intent intent) {
-		        String action = intent.getAction();
-		        // When discovery finds a device
-		        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-		            // Get the BluetoothDevice object from the Intent
-		            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-		            Log.d("DeviceList" , device.getName() + "\n" + device.getAddress());
-		            btDevice = device;
-		            // Add the name and address to an array adapter to show in a ListView
-		            //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-		        }
+	public void getDevice() {		
+		// If there are paired devices
+		if (pairedDevices.size() > 0) {
+		    // Loop through paired devices
+		    for (BluetoothDevice device : pairedDevices) {
+		        // Add the name and address to an array adapter to show in a ListView
+		        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
 		    }
-		};			
 		}
-		
-		// Register the BroadcastReceiver
-		//IntentFilter filter = new IntentFilter(BluetoothDevicxxe.ACTION_FOUND);
-		//registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-		
 	}
-	
-//	public void turnBluetoothOn(){
-//		if (!isBluetoothEnabled()) {
-//			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-//		}
-//	}	
 
-	public void connect(){
+	public void connect(BluetoothDevice device){
+		btDevice = device;
 		// check if there is a connection : terminate
-		getDevice();
 		if(ct != null) {
 			ct.cancel();
 		}
@@ -112,8 +122,22 @@ public class RemoteBluetoothClient{
 		}else{
 			//TODO message: no connectedThread. (connect() to start a new connection)
 		}		
-	}
+	}	
 	
+	//FIX THE RECEIVER!
+	// Create a BroadcastReceiver for ACTION_FOUND
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        // When discovery finds a device
+	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+	            // Get the BluetoothDevice object from the Intent
+	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	            // Add the name and address to an array adapter to show in a ListView
+	            mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+	        }
+	    }
+	};
 	
 	//creates a socket and connects to the server
 	private class ConnectThread extends Thread{
@@ -123,11 +147,8 @@ public class RemoteBluetoothClient{
 		public ConnectThread(BluetoothDevice device){
 			btDevice = device;
 			try{
-				btSocket = btDevice.createInsecureRfcommSocketToServiceRecord(uuid.fromString("04c6093b-0000-1000-8000-00805f9b34fb"));
-				
-			}catch(IOException e){
-		
-			}
+				btSocket = btDevice.createInsecureRfcommSocketToServiceRecord(uuid);				
+			}catch(IOException e){}
 		}
 		
 		public void run(){
@@ -137,10 +158,7 @@ public class RemoteBluetoothClient{
 			} catch (IOException e) {
 				try{
 					btSocket.close();
-				}catch(IOException ex){
-					
-				}
-				e.printStackTrace();
+				}catch(IOException ex){}
 			}
 			connected(btDevice,btSocket);
 		}
@@ -148,15 +166,12 @@ public class RemoteBluetoothClient{
 		public void cancel(){
 			try {
 				btSocket.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			} catch (IOException e) {}
 		}
 
 	}
 
-	//uses the created 
+	//uses the created connection
 	private class ConnectedThread extends Thread{
 		BluetoothSocket btSocket;
 		OutputStream os;
@@ -166,29 +181,24 @@ public class RemoteBluetoothClient{
 			os = null;
 			try{
 				os = socket.getOutputStream();
-			}catch(IOException e){
-				//fill me out!	
-			}
+			}catch(IOException e){}
 		}
+		
 		public void run(){
 			byte[] buffer = new byte[1024];
 			int bytes;
 		}
+		
 		public void write(byte[] buffer){
 			try {
 				os.write(buffer);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			} catch (IOException e) {}
 		}
+		
 		public void cancel(){
 			try {
 				btSocket.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			} catch (IOException e) {}
 		}
 	}
 }
