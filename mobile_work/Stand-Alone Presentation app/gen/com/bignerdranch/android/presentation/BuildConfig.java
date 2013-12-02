570a6109e2d1466c817bf8cf0f55d310f4aefce3
pkosto <pkosto@essex.ac.uk>
2013-12-02 03:26:39 +0000

    This is a Stand-Alone Presentation application. It contains errors and has to be refined but it should suffice as ground work for the rest of the Stand-Alone applications and the main one.

---
+++ mobile_work/Stand-Alone Presentation app/gen/com/bignerdranch/android/presentation/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bignerdranch.android.presentation;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
