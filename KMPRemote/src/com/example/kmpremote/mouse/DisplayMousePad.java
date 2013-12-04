a7b271fbad08f8564e371e3d6e8af33a65c6d0ce
lgoodl <lgoodl@essex.ac.uk>
2013-12-04 15:51:19 +0000

    new project for everyone to work from

---
+++ KMPRemote/src/com/example/kmpremote/mouse/DisplayMousePad.java
@@ -0,0 +1,86 @@
+package com.example.kmpremote.mouse;
+
+import com.example.kmpremote.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class DisplayMousePad extends Activity {
+	
+	//ConnectedThread t;
+	public static String cmd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mousepad_display);
+        //mclient = new RemoteBluetoothClient();
+        //mclient.mArrayAdapter = new ArrayAdapter<String>(this,R.layout.mousepad_display);
+        //ListView newDevicesListView = (ListView)()
+        //mclient.connect();
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    //need to still implement these methods to send data to the bluetooth client class!
+    public void moveUp(View v){
+    	//call the bluetooth class and send MOVE_UP command
+    	cmd = "MOUSE MOVE UP 20";
+    	byte[] buffer = cmd.getBytes();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
+    	
+    }
+    
+    public void moveDown(View v){
+    	cmd = "MOUSE MOVE DOWN 20";
+    	byte[] buffer = cmd.getBytes();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
+    	
+    	
+    }
+
+	public void moveLeft(View v){
+		String cmd = "MOUSE MOVE LEFT 20";
+    	byte[] buffer = cmd.getBytes();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
+	
+	}
+
+	public void moveRight(View v){
+		String cmd = "MOUSE MOVE RIGHT 20";
+    	byte[] buffer = cmd.getBytes();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
+	
+	}
+
+	public void leftClick(View v){
+		String cmd = "MOUSE LEFT CLICK";
+    	byte[] buffer = cmd.getBytes();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+ cmd, Toast.LENGTH_LONG).show();
+	
+	}
+
+	public void rightClick(View v){
+		String cmd = "MOUSE RIGHT CLICK";
+    	byte[] buffer = cmd.getBytes();
+    	//mclient.send(buffer);
+    	Toast.makeText(this,  "Message sent!"+cmd, Toast.LENGTH_LONG).show();
+	
+	}
+    
+}
---
+++ KMPRemote/src/com/example/kmpremote/presentation/PresentationActivity.java
@@ -0,0 +1,70 @@
+package com.example.kmpremote.presentation;
+
+import com.example.kmpremote.R;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class PresentationActivity extends Activity {
+	OnSwipeTouchListener fswipe;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_presentation);
+	}
+
+	
+
+	public void previousSlide(View v) {
+		String theKey = "KEYBOARD TOGGLE LEFTARROW ";
+		byte[] buffer = theKey.getBytes();
+		// RemoteBluetoothClient.send(buffer);
+		Toast.makeText(PresentationActivity.this, R.string.Left_Toast,
+				Toast.LENGTH_SHORT).show();
+	}
+
+	public void startPresentation(View v) {
+		String theKey = "KEYBOARD TOGGLE F5 ";
+		byte[] buffer = theKey.getBytes();
+		// RemoteBluetoothClient.send(buffer);
+		Toast.makeText(PresentationActivity.this, R.string.F5_Toast,
+				Toast.LENGTH_SHORT).show();
+	}
+
+	public void exitPresentation(View v) {
+		String theKey = "KEYBOARD TOGGLE ESC ";
+		byte[] buffer = theKey.getBytes();
+		// RemoteBluetoothClient.send(buffer);
+		Toast.makeText(PresentationActivity.this, R.string.ESC_Toast,
+				Toast.LENGTH_SHORT).show();
+	}
+
+	public void nextSlide(View v) {
+		String theKey = "KEYBOARD TOGGLE RIGHTARROW ";
+		byte[] buffer = theKey.getBytes();
+		// RemoteBluetoothClient.send(buffer);
+		Toast.makeText(PresentationActivity.this, R.string.Right_Toast,
+				Toast.LENGTH_SHORT).show();
+	}
+
+	public void swipe(View v) {
+		v.setOnTouchListener(new OnSwipeTouchListener() {
+			public void onSwipeRight() {
+				Toast.makeText(PresentationActivity.this, R.string.Right_Toast,
+						Toast.LENGTH_SHORT).show();
+			}
+
+			public void onSwipeLeft() {
+				Toast.makeText(PresentationActivity.this, R.string.Left_Toast,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+}
