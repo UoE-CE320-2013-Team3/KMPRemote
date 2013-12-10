3f1a9c281f9520c61a4d4fb56b76ea8f760d4d90
vkujum <vkujum@essex.ac.uk>
2013-12-10 15:51:39 +0000

    Corrections by jwpulf:  - Created stringToDelimitedBytes()
    Corrections by pkosto & vkujum:
     - some reformatting done
     - added inputstream to fetch data from server
     - getJSONObject() to send JSON Object on request

--- KMPRemote/src/com/example/kmpremote/bluetoothclient/RemoteBluetoothClient.java
+++ KMPRemote/src/com/example/kmpremote/bluetoothclient/RemoteBluetoothClient.java
@@ -3,11 +3,12 @@ package com.example.kmpremote.bluetoothclient;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
@@ -19,9 +20,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnKeyListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -36,57 +35,55 @@ import com.example.kmpremote.presentation.PresentationActivity;
 
 public class RemoteBluetoothClient extends Activity{
 	static UUID uuid = UUID.fromString("04c6093b-0000-1000-8000-00805f9b34fb");
-	
+
 	//Variables for the adapter(phone's Bluetooth) and device(server Bluetooth).
 	private	BluetoothAdapter deviceBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 	private BluetoothDevice btDevice;
-	private BluetoothSocket btSocket;
-	private static String testcmdBytes= ""; 
 	private	Set<BluetoothDevice> pairedDevices = deviceBluetoothAdapter.getBondedDevices();	
 	Context context;
-	
+
 	public ArrayAdapter<String> newArrayAdapter;
 	public ArrayAdapter<String> pairedArrayAdapter;
-	
-	
+
 	//threads
 	private ConnectThread makeCnt; //thread that creates the connection
 	private static ConnectedThread maintainCnt; //thread that manages the connection
-	
+
 	public void mouseActivity(View v){
 		Intent mouse = new Intent(this, DisplayMousePad.class);
 		startActivity(mouse);
 	}
-	
+
 	public void keyboardActivity(View v){
 		Intent keyboard = new Intent(this, KeyboardActivity.class);
 		startActivity(keyboard);
 	}
-	
+
 	public void presentationActivity(View v){
 		Intent pres = new Intent(this, PresentationActivity.class);
 		startActivity(pres);
 	}
 	public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bluetooth);
 		//Initialise array containing newly found devices devices
 		newArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
-		
+		pairedArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
+
 		//Creating ListView for the two arrays
 		ListView pairedDevicesListView = (ListView) findViewById(R.id.listView2);
 		pairedDevicesListView.setAdapter(pairedArrayAdapter);
-		
+
 		ListView newDevicesListView = (ListView)findViewById(R.id.listView1);
 		newDevicesListView.setAdapter(newArrayAdapter);
-		
+
 		//creating two intents and registers
 		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
 		registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy		
-		
+
 		filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
 		registerReceiver(mReceiver, filter);
-		
+
 		if(hasBluetoothAdapter()){
 			if(!isBluetoothEnabled()){
 				Toast.makeText(getApplicationContext(), "No Bluetooth enabled", Toast.LENGTH_SHORT).show();
@@ -96,72 +93,53 @@ public class RemoteBluetoothClient extends Activity{
 			Toast.makeText(getApplicationContext(), "The device has no Bluetooth", Toast.LENGTH_SHORT).show();				
 		}			
 		getDevice();
-		
-		
-		
-		newDevicesListView.setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
+		newDevicesListView.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
+
 				//Get the mac address of the selected device.
 				String info = ((TextView) arg1).getText().toString();
-				//TODO FIX THIS HARDCODE 
 				String address = info.substring(info.length() - 17);
-				//String address = "00:0A:5D:80:85";
-				
+
 				btDevice = deviceBluetoothAdapter.getRemoteDevice(address);
 				connect();			
-				send(testcmdBytes);
 			}			
 		});
-		
-		newDevicesListView.setOnKeyListener(new OnKeyListener(){
-
-			@Override
-			public boolean onKey(View arg0, int arg1, KeyEvent arg2) {
-				// TODO Auto-generated method stub
-				if (arg1 == KeyEvent.KEYCODE_VOLUME_DOWN){
-					send(testcmdBytes);
-				}
-				if (arg1 == KeyEvent.KEYCODE_VOLUME_UP){
-					send(testcmdBytes);
-				}
-				return false;
-			}});
 	}
-	
+
 	private boolean hasBluetoothAdapter(){
 		if (deviceBluetoothAdapter == null) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	public boolean isBluetoothEnabled(){	
 		return deviceBluetoothAdapter.isEnabled();
 	}	
-	
+
 	public void getDevice() {	
-		
+
 		// If there are paired devices
 		if (pairedDevices.size() > 0) {
 			findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
-		    // Loop through paired devices
-		    for (BluetoothDevice device : pairedDevices) {
-		        // Add the name and address to an array adapter to show in a ListView
-		        newArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-		    }
+			// Loop through paired devices
+			for (BluetoothDevice device : pairedDevices) {
+				// Add the name and address to an array adapter to show in a ListView
+				newArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+			}
 		}else {
 			String noDevices = getResources().getText(R.string.none_paired).toString();
 			newArrayAdapter.add(noDevices);
 		}
 	}
-	
+
 	//Method that searches for devices
 	private void discoverDevices() {
 		// Turn on sub-title for new devices
 		findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
+		findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
 
 		// If we're already discovering, stop it
 		if (deviceBluetoothAdapter.isDiscovering()) {
@@ -170,9 +148,9 @@ public class RemoteBluetoothClient extends Activity{
 		// Request discover from BluetoothAdapter
 		deviceBluetoothAdapter.startDiscovery();
 	}
-	
+
 	//Method that starts the connection thread
-		public void connect(){
+	public void connect(){
 		// check if there is a connection : terminate
 		if(makeCnt != null) {
 			makeCnt.cancel();
@@ -180,7 +158,7 @@ public class RemoteBluetoothClient extends Activity{
 		makeCnt = new ConnectThread(btDevice);
 		makeCnt.start();				
 	}
-	
+
 	//Method that maintains the connection
 	private void connected(BluetoothDevice device, BluetoothSocket socket){
 		// if to cancel the connection thread
@@ -189,44 +167,54 @@ public class RemoteBluetoothClient extends Activity{
 		}
 		maintainCnt = new ConnectedThread(socket);
 		maintainCnt.start();
-		/*
-		Intent i = new Intent(getApplicationContext(), PresentationActivity.class);
-		startActivity(i);*/
 	}
-	
-	//Public send method for other classes to use when sending over to the server
-	public static void send(String cmd) {
-		//If there is a connection : send
+
+	//Adding end command to each byte array 
+	private static byte[] stringToDelimitedBytes(String cmd) {
 		byte[] commandBuffer = cmd.getBytes();
 		byte[] commandBufferWithEnding= new byte[commandBuffer.length+1];
 		System.arraycopy(commandBuffer, 0, commandBufferWithEnding, 0, commandBuffer.length);
+		commandBufferWithEnding[commandBufferWithEnding.length-1] = -2;
+		return commandBufferWithEnding;
+	}
+
+	//Public send method for other classes to use when sending over to the server
+	public static void send(String cmd) {
+		//If there is a connection : send
+		byte[] commandBufferWithEnding= stringToDelimitedBytes(cmd);
 		if(maintainCnt != null) {
-		    commandBufferWithEnding[commandBufferWithEnding.length-1] = -2;
 			maintainCnt.write(commandBufferWithEnding);
-		}else{
-//			Toast.makeText(getApplicationContext(), "No active connection", Toast.LENGTH_SHORT).show();
-		}		
+		}					
 	}	
+
+	//Get the JSONObject containing the notes, be aware that the Object might not be populated if the data has not been read.
+	public static JSONObject getJSONObject() {
+		if (maintainCnt.jObj == null) {
+			return null;
+		} else {
+			return maintainCnt.jObj;
+		}
+	}
 	
 	//BroadcastReceiver for ACTION_Found
 	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-	    public void onReceive(Context context, Intent intent) {
-	        String action = intent.getAction();
-	        // When discovery finds a device
-	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-	            // Get the BluetoothDevice object from the Intent
-	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-	            // Add the name and address to an array adapter to show in a ListView
-	            newArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-	        }
-	    }
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			// When discovery finds a device
+			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+				// Get the BluetoothDevice object from the Intent
+				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+				// Add the name and address to an array adapter to show in a ListView
+				newArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+			}
+		}
 	};
-	
+
 	//Thread that creates a socket and connects to the server
 	private class ConnectThread extends Thread{
 		private BluetoothDevice btDevice;
 		private BluetoothSocket btSocket;
-		
+
 		public ConnectThread(BluetoothDevice device){
 			btDevice = device;
 			try{
@@ -235,11 +223,11 @@ public class RemoteBluetoothClient extends Activity{
 				Toast.makeText(getApplicationContext(), "Error at socketing", Toast.LENGTH_SHORT).show();
 			}
 		}
-		
+
 		public void run(){
 			if (deviceBluetoothAdapter.isDiscovering()) {
-			deviceBluetoothAdapter.cancelDiscovery();
-		}
+				deviceBluetoothAdapter.cancelDiscovery();
+			}
 			try {
 				btSocket.connect();
 			} catch (IOException e) {
@@ -251,7 +239,7 @@ public class RemoteBluetoothClient extends Activity{
 			}
 			connected(btDevice,btSocket);
 		}
-		
+
 		public void cancel(){
 			try {
 				btSocket.close();
@@ -261,11 +249,13 @@ public class RemoteBluetoothClient extends Activity{
 		}
 
 	}
+
 	//Thread to maintain the connection and send data to the server
 	private class ConnectedThread extends Thread{
 		BluetoothSocket btSocket;
 		OutputStream os;
 		InputStream is;
+		JSONObject jObj = new JSONObject();
 		
 		ConnectedThread(BluetoothSocket socket){
 			btSocket = socket;
@@ -276,50 +266,72 @@ public class RemoteBluetoothClient extends Activity{
 				Toast.makeText(getApplicationContext(), "Error at outputing", Toast.LENGTH_SHORT).show();
 			}
 		}
-		
+
 		public void run(){
 			if (deviceBluetoothAdapter.isDiscovering()) {
-			deviceBluetoothAdapter.cancelDiscovery();
-		}
+				deviceBluetoothAdapter.cancelDiscovery();
+			}
 			byte[] buffer = new byte[1024]; 
-			int bytes = 0;
-			
+			int bytes = 0;			
 			// Keep listening to the InputStream while connected
-            while(true) {
+			while(true) {
 				listen(buffer, bytes);
 			}
-		}
-		
+		}		
+
+		//listen method that stores the received bytes to an array and then sends it on.
 		public void listen(byte[] buffer, int bytes) {
-			JSONObject jObj = new JSONObject();
-			String end = "-2";
+			int end = -2;
+			int bye = -1;
 			
-			while(true) {
-				try {
-					bytes = is.read(buffer);
-					String input = jObj.getString(new String(buffer, "UTF-8"));
-					System.out.println("Input: " + input);
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-					break;
-				} catch (JSONException e) {
-					e.printStackTrace();
-				} 	
+			List<Byte> isBytes = new ArrayList<Byte>();
+			while(true){
+				try{
+					byte aByte = (byte) is.read();
+					if(aByte == (end)){
+						try{
+							byte[] arrBytes = new byte[isBytes.size()];
+							int i = 0;
+							for(Byte b :isBytes){
+								arrBytes[i++] = b;
+							}
+							String serverInput = new String(arrBytes);
+							jObj.put("notes", serverInput);
+//							System.out.println("Server sent: " + serverInput);
+							System.out.println("Server sent (jObj): " + jObj.getString("notes"));
+						}catch(Exception e){
+							e.printStackTrace();
+						}
+					}else if(aByte == bye){
+						break;
+					}else{
+						isBytes.add(aByte);
+					}
+				}catch(Exception e){
+					e.printStackTrace();	
+				}
 			}
 		}
 		
-		public void write(byte[] buffer) {
-			
+		// Send byte array to the server
+		public void write(byte[] buffer) {			
+//			String test = "PRESENTATION LINK C:\\Users\\Pete\\Desktop\\test.ppt END_LINK";
+//			String notes = "PRESENTATION NOTES";			
 			try {
-				os.write(buffer);
+				// working code
+				os.write(stringToDelimitedBytes(buffer));
 				os.flush();
+
+				// testing code
+//				os.write(stringToDelimitedBytes(test));
+//				os.flush();
+//				os.write(stringToDelimitedBytes(notes));
+//				os.flush();
 			} catch (IOException e) { 
 				Toast.makeText(getApplicationContext(), "Error at writing", Toast.LENGTH_SHORT).show();
-				}
+			}
 		}
-		
+
 		public void cancel(){
 			try {
 				btSocket.close();
