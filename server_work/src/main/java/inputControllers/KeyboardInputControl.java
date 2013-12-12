75aca29cbeea08ae64c8d1ef651a59b4b555d00f
PulfordJ <jwpulf@essex.ac.uk>
2013-12-12 14:11:45 +0000

    Fixed full stop, command KEYBOARD TOGGLE . now works. Tested with lgoodl

--- server_work/src/main/java/inputControllers/KeyboardInputControl.java
+++ server_work/src/main/java/inputControllers/KeyboardInputControl.java
@@ -63,29 +63,33 @@ public class KeyboardInputControl {
 
         for (String string : stringArray) {
 
-           //if character
-           if (string.length() == 1) {
-               character = string.charAt(0);
-               keyCode = returnKeyCodeForCharacter(character);
+            //if character
 
-               if (Character.isUpperCase(character)) {
-                   robot.keyPress(KeyEvent.VK_SHIFT);
-               }
+            character = string.charAt(0);
 
-               robot.keyPress(keyCode);
-               robot.keyRelease(keyCode);
+            if (string.length() == 1 && Character.isLetter(character) | Character.isDigit(character)) {
 
-               if (Character.isUpperCase(character)) {
-                   robot.keyRelease(KeyEvent.VK_SHIFT);
-               }
 
-           }
-           //if string
-           else {
-               keyCode = returnKeyCodeForString(string);
-               robot.keyPress(keyCode);
-               robot.keyRelease(keyCode);
-           }
+                keyCode = Character.toUpperCase(character);
+
+                if (Character.isUpperCase(character)) {
+                    robot.keyPress(KeyEvent.VK_SHIFT);
+                }
+
+                robot.keyPress(keyCode);
+                robot.keyRelease(keyCode);
+
+                if (Character.isUpperCase(character)) {
+                    robot.keyRelease(KeyEvent.VK_SHIFT);
+                }
+
+            }
+            //if string
+            else {
+                keyCode = returnKeyCodeForString(string);
+                robot.keyPress(keyCode);
+                robot.keyRelease(keyCode);
+            }
         }
     }
 
@@ -94,7 +98,7 @@ public class KeyboardInputControl {
 
         char keyCode;
 
-        for (String string: stringArray) {
+        for (String string : stringArray) {
             keyCode = returnKeyCodeForString(string);
 
             robot.keyPress(keyCode);
@@ -124,8 +128,7 @@ public class KeyboardInputControl {
     public void wait(int milliseconds) {
         try {
             Thread.sleep(milliseconds);
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
@@ -136,9 +139,7 @@ public class KeyboardInputControl {
 
             return Character.toUpperCase(character);
 
-        }
-
-        else {
+        } else {
             throw new NoSuchKeyException(character);
         }
     }
@@ -147,158 +148,107 @@ public class KeyboardInputControl {
     private char returnKeyCodeForString(String string) throws NoSuchKeyException {
         if (string.equals("SHIFT")) {
             return KeyEvent.VK_SHIFT;
-        }
-        else if (string.equals("CTRL")) {
+        } else if (string.equals("CTRL")) {
             return KeyEvent.VK_CONTROL;
-        }
-        else if (string.equals("ALT")) {
+        } else if (string.equals("ALT")) {
             return KeyEvent.VK_ALT;
-        }
-        else if (string.equals("TAB")) {
+        } else if (string.equals("TAB")) {
             return KeyEvent.VK_TAB;
-        }
-        else if (string.equals("SPACE")) {
+        } else if (string.equals("SPACE")) {
             return KeyEvent.VK_SPACE;
-        }
-        else if (string.equals("CAPS")) {
+        } else if (string.equals("CAPS")) {
             return KeyEvent.VK_CAPS_LOCK;
-        }
-        else if (string.equals("ENTER")) {
+        } else if (string.equals("ENTER")) {
             return KeyEvent.VK_ENTER;
-        }
-        else if (string.equals("BACKSPACE")) {
+        } else if (string.equals("BACKSPACE")) {
             return KeyEvent.VK_BACK_SPACE;
-        }
-        else if (string.equals("F1")) {
+        } else if (string.equals("F1")) {
             return KeyEvent.VK_F1;
-        }
-		else if (string.equals("F2")) {
+        } else if (string.equals("F2")) {
             return KeyEvent.VK_F2;
-        }
-		else if (string.equals("F3")) {
+        } else if (string.equals("F3")) {
             return KeyEvent.VK_F3;
-        }
-		else if (string.equals("F4")) {
+        } else if (string.equals("F4")) {
             return KeyEvent.VK_F4;
-        }
-		else if (string.equals("F5")) {
+        } else if (string.equals("F5")) {
             return KeyEvent.VK_F5;
-        }
-		else if (string.equals("F6")) {
+        } else if (string.equals("F6")) {
             return KeyEvent.VK_F6;
-        }
-		else if (string.equals("F7")) {
+        } else if (string.equals("F7")) {
             return KeyEvent.VK_F7;
-        }
-		else if (string.equals("F8")) {
+        } else if (string.equals("F8")) {
             return KeyEvent.VK_F8;
-        }
-		else if (string.equals("F9")) {
+        } else if (string.equals("F9")) {
             return KeyEvent.VK_F9;
-        }
-		else if (string.equals("F10")) {
+        } else if (string.equals("F10")) {
             return KeyEvent.VK_F10;
-        }
-		else if (string.equals("F11")) {
+        } else if (string.equals("F11")) {
             return KeyEvent.VK_F11;
-        }
-		else if (string.equals("F12")) {
+        } else if (string.equals("F12")) {
             return KeyEvent.VK_F12;
-        }
-        else if (string.equals("RIGHTARROW")) {
+        } else if (string.equals("RIGHTARROW")) {
             return KeyEvent.VK_RIGHT;
-        }
-        else if (string.equals("LEFTARROW")) {
+        } else if (string.equals("LEFTARROW")) {
             return KeyEvent.VK_LEFT;
-        }
-		else if (string.equals("UPARROW")) {
+        } else if (string.equals("UPARROW")) {
             return KeyEvent.VK_UP;
-        }
-        else if (string.equals("DOWNARROW")) {
+        } else if (string.equals("DOWNARROW")) {
             return KeyEvent.VK_DOWN;
-        }
-        else if (string.equals("PAGEUP")) {
+        } else if (string.equals("PAGEUP")) {
             return KeyEvent.VK_PAGE_UP;
-        }
-        else if (string.equals("PAGEDOWN")) {
+        } else if (string.equals("PAGEDOWN")) {
             return KeyEvent.VK_PAGE_DOWN;
-        }
-        else if (string.equals("ESC")) {
+        } else if (string.equals("ESC")) {
             return KeyEvent.VK_ESCAPE;
-        }
-		else if (string.equals(".")) {
+        } else if (string.equals(".")) {
             return KeyEvent.VK_PERIOD;
-        }
-		else if (string.equals(",")) {
+        } else if (string.equals(",")) {
             return KeyEvent.VK_COMMA;
-        }
-		else if (string.equals("/")) {
+        } else if (string.equals("/")) {
             return KeyEvent.VK_SLASH;
-        }
-		else if (string.equals("\\")) {
+        } else if (string.equals("\\")) {
             return KeyEvent.VK_BACK_SLASH;
-        }
-		else if (string.equals(":")) {
+        } else if (string.equals(":")) {
             return KeyEvent.VK_COLON;
-        }
-		else if (string.equals(";")) {
+        } else if (string.equals(";")) {
             return KeyEvent.VK_SEMICOLON;
-        }
-		else if (string.equals("=")) {
+        } else if (string.equals("=")) {
             return KeyEvent.VK_EQUALS;
-        }
-		else if (string.equals("\"")) {
+        } else if (string.equals("\"")) {
             return KeyEvent.VK_QUOTEDBL;
-        }
-		else if (string.equals("<")) {
+        } else if (string.equals("<")) {
             return KeyEvent.VK_LESS;
-        }
-		else if (string.equals(">")) {
+        } else if (string.equals(">")) {
             return KeyEvent.VK_GREATER;
-        }
-		else if (string.equals("&")) {
+        } else if (string.equals("&")) {
             return KeyEvent.VK_AMPERSAND;
-        }
-		else if (string.equals("@")) {
+        } else if (string.equals("@")) {
             return KeyEvent.VK_AT;
-        }
-		else if (string.equals("!")) {
+        } else if (string.equals("!")) {
             return KeyEvent.VK_EXCLAMATION_MARK;
-        }
-		else if (string.equals("#")) {
+        } else if (string.equals("#")) {
             return KeyEvent.VK_NUMBER_SIGN;
-        }
-		else if (string.equals("\'")) {
+        } else if (string.equals("\'")) {
             return KeyEvent.VK_QUOTE;
-        }
-		else if (string.equals("(")) {
+        } else if (string.equals("(")) {
             return KeyEvent.VK_LEFT_PARENTHESIS;
-        }
-		else if (string.equals(")")) {
+        } else if (string.equals(")")) {
             return KeyEvent.VK_RIGHT_PARENTHESIS;
-        }
-		else if (string.equals("*")) {
+        } else if (string.equals("*")) {
             return KeyEvent.VK_ASTERISK;
-        }
-		else if (string.equals("+")) {
+        } else if (string.equals("+")) {
             return KeyEvent.VK_PLUS;
-        }
-		else if (string.equals("-")) {
+        } else if (string.equals("-")) {
             return KeyEvent.VK_MINUS;
-        }
-		else if (string.equals("[")) {
+        } else if (string.equals("[")) {
             return KeyEvent.VK_OPEN_BRACKET;
-        }
-		else if (string.equals("]")) {
+        } else if (string.equals("]")) {
             return KeyEvent.VK_CLOSE_BRACKET;
-        }
-		else if (string.equals("_")) {
+        } else if (string.equals("_")) {
             return KeyEvent.VK_UNDERSCORE;
-        }
-		else if (string.equals("$")) {
+        } else if (string.equals("$")) {
             return KeyEvent.VK_DOLLAR;
-        }
-        else {
+        } else {
             throw new NoSuchKeyException(string);
         }
 
@@ -307,12 +257,13 @@ public class KeyboardInputControl {
     public class NoSuchKeyException extends RuntimeException {
         private NoSuchKeyException(char character) {
             super(character + "is not a valid character. Character must " +
-                  "be in range [a-Z] or [1-9]");
+                    "be in range [a-Z] or [1-9]");
         }
+
         private NoSuchKeyException(String string) {
             super(string + "is not a valid string. Valid strings include:\n" +
-                  "SHIFT\nCTRL\nALT\nTAB\nSPACE\nCAPS\nENTER\nBACKSPACE\n" +
-                  "F5\nF6\nRIGHTARROW\nLEFTARROW\nPAGEUP\nPAGEDOWN\nESC\n");
+                    "SHIFT\nCTRL\nALT\nTAB\nSPACE\nCAPS\nENTER\nBACKSPACE\n" +
+                    "F5\nF6\nRIGHTARROW\nLEFTARROW\nPAGEUP\nPAGEDOWN\nESC\n");
 
         }
     }
