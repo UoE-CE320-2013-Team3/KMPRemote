6c6a15c0e86ac8e9950f10fa36766ee1eb6f246d
PulfordJ <jwpulf@essex.ac.uk>
2013-12-12 12:29:49 +0000

    Added jFrame that is closed to terminate the server.

--- server_work/src/main/java/bluetooth/RemoteBluetoothServer.java
+++ server_work/src/main/java/bluetooth/RemoteBluetoothServer.java
@@ -1,5 +1,8 @@
 package main.java.bluetooth;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: john
@@ -10,6 +13,19 @@ package main.java.bluetooth;
 // Built on demonstration code found here: http://luugiathuy.com/2011/02/android-java-bluetooth/comment-page-2/
 public class RemoteBluetoothServer {
     public static void main(String[] args) {
+        // Frame code based on example in:  http://docs.oracle.com/javase/tutorial/uiswing/components/frame.html
+        JFrame frame = new JFrame("Bluetooth Remote Server");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create components and put them in the frame.
+        JLabel instructions = new JLabel("Close this window when you no longer want to be able to remotely control your device.");
+        frame.getContentPane().add(instructions, BorderLayout.CENTER);
+
+        //Size the frame.
+        frame.pack();
+
+        // Show it.
+        frame.setVisible(true);
         Thread waitThread = new Thread(new WaitThread());
         waitThread.start();
 
